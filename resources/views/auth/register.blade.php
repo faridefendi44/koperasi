0f6fa7f5d83fa9cee6f069444f86f970e58b1e57
Eci Emriyeni <eciemriyeni3@gmail.com>
2024-07-15 12:32:13 +0700

    edit placeholder

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,7 +24,7 @@
                 <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 ">Email</label>
                 <input type="text" name="email"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 h-12"
-                    placeholder="Ketik username" required />
+                    placeholder="Ketik email" required />
             </div>
             <div>
                 <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 ">Nomor Whatsapp</label>
